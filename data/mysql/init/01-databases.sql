73f868fc11d35758a66db2f5f70387d162afb469
rgonzales <ronelgonzales5@gmail.com>
2024-09-08 08:36:47 +0800

    update

--- data/mysql/init/01-databases.sql
+++ data/mysql/init/01-databases.sql
@@ -2,8 +2,12 @@
 set global sql_mode='';
 
 #create required users
-GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'%' IDENTIFIED BY 'loader';
-GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'127.0.0.1' IDENTIFIED BY 'loader';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '';
+#GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'%' IDENTIFIED BY 'loader';
+#GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'localhost' IDENTIFIED BY 'loader';
+#GRANT ALL PRIVILEGES ON *.* TO 'womenandinfants'@'%' IDENTIFIED BY 'milk22';
+#GRANT ALL PRIVILEGES ON *.* TO 'womenandinfants'@'localhost' IDENTIFIED BY 'milk22';
+#GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'loader';
+#GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'loader'
 
+CREATE DATABASE IF NOT EXISTS platform;
+CREATE DATABASE IF NOT EXISTS platform_dw;
