5df8116041d6f7bce1322100cabd4167aabdc281
ronel gonzales <ronelgonzales5@gmail.com>
2026-06-18 07:50:35 +0800

    Update docker-compose.yml

--- data/mysql/init/01-databases.sql
+++ data/mysql/init/01-databases.sql
@@ -1,6 +1,10 @@
 #disable strict mode
 set global sql_mode='';
 
+# Change root user authentication to mysql_native_password for compatibility
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+
 #create required users
 #GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'%' IDENTIFIED BY 'loader';
 #GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'localhost' IDENTIFIED BY 'loader';
